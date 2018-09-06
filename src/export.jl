9df833c79c38bc9852662abdce144aeb73eb6339
Nathanael Schilling <schillna@ma.tum.de>
2018-09-06 20:28:55 +0200

    Export more

--- src/export.jl
+++ src/export.jl
@@ -2,6 +2,7 @@ export
    fast_trilinear_earth_interpolate,
    fast_tricubic_earth_interpolate,
    fast_tricubic_earth_interpolate_gradient,
+   fast_tricubic_earth_interpolate_eqvari,
    sshVelocityRHS,
    read_ocean_velocities,
    read_ssh
