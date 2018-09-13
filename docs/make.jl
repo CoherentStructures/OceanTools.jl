038b03e49bacea977e0d9cc6e206b83171a20266
Nathanael Schilling <schillna@ma.tum.de>
2018-09-13 22:20:27 +0200

    CI files

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+Base.HOME_PROJECT[] = abspath(Base.HOME_PROJECT[]) # JuliaLang/julia/pull/28625
+ENV["GKSwstype"] = "100"
+
+using Documenter, CopernicusUtils
+
+#Before running this, make sure that Plots, Tensors, Distances,JLD2, Printf, Random, OrdinaryDiffEq and Clustering packages are
+#installed and added to your current environment (]add )
+
+makedocs(
+    format=:html,
+    sitename="CopernicusUtils.jl",
+    pages = Any[
+        "Home" => "index.md"
+        "Examples" => "examples.md"
+    ]
+    )
+
+deploydocs(
+    repo = "github.com/CoherentStructures/CopernicusUtils.jl.git",
+    target = "build",
+    julia = "1.0",
+    deps = nothing,
+    make = nothing,
+)
