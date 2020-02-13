827bfa0936c332949bb0acd6af8f72bfac02a260
Nathanael Schilling <schillna@ma.tum.de>
2020-02-13 13:50:34 +0100

    Fix bugs

--- src/dataformat.jl
+++ src/dataformat.jl
@@ -38,18 +38,22 @@ end
 
 Convert lon-lat velocities `U` and `V` from units degrees/second to kilometers/day.
 Save the result in Us[:,:,numfound] (resp. Vs[:,:,numfound])
+The numbers lli,llj are expected to refer to zero-based indexes!
 """
-function rescaleUV(U, V, Lon, Lat,Us,Vs,numfound,remove_nan)
+function rescaleUV(U, V, Lon, Lat,Us,Vs,numfound,remove_nan,lli,llj)
     R = 6371e3 # radius of the Earth in kilometers
     s =3600*24/R
-    n = size(U)[1]
-    m = size(U)[2]
+    n_small = size(Us,1)
+    m_small = size(Vs,2)
+    n_full = size(U,1)
+    m_full = size(V,2)
     missing_value = remove_nan ? 0.0 : NaN
-    for j in 1:m
-        for i in 1:n
-            Us[i,j,numfound] = ismissing(U[i,j]) ? missing_value :  sec(deg2rad(Lat[j])) * rad2deg(U[i,j]) * s
-            Vs[i,j,numfound] = ismissing(V[i,j]) ? missing_value : rad2deg(V[i,j]) * s
-        end
+    
+    for i in 1:n_small, j in 1:m_small
+        fulli = mod1(lli+i,n_full)
+        fullj = mod1(llj+j,m_full)
+            Us[i,j,numfound] = ismissing(U[fulli,fullj]) ? missing_value :  sec(deg2rad(Lat[fullj])) * rad2deg(U[fulli,fullj]) * s
+            Vs[i,j,numfound] = ismissing(V[fulli,fullj]) ? missing_value : rad2deg(V[fulli,fullj]) * s
     end
 end
 
@@ -60,7 +64,9 @@ function read_ocean_velocities(howmany, ww_ocean_data;
                                 arraycons=SharedArray{Float64},
                                 LL_space=nothing,UR_space=nothing
                                 )
-    Lon, Lat = getLonLat(ww_ocean_data * "/" * readdir(ww_ocean_data)[1])
+    Lon32, Lat32 = getLonLat(ww_ocean_data * "/" * readdir(ww_ocean_data)[1])
+    Lon = Float64.(Lon32)
+    Lat = Float64.(Lat32)
     nx_full = length(Lon)
     ny_full = length(Lat)
 
@@ -68,8 +74,8 @@ function read_ocean_velocities(howmany, ww_ocean_data;
 
     bounds_given =  (LL_space != nothing)
 
-    LL = bounds_given ? clone(LL_space) : [Lon[1], Lat[1]] 
-    UR = bounds_given ? clone(UR_space) : [Lon[1] + 360, Lat[1] + 180]
+    LL = bounds_given ? copy(LL_space) : [Lon[1], Lat[1]] 
+    UR = bounds_given ? copy(UR_space) : [Lon[1] + 360, Lat[1] + 180]
 
     function periodicGoLeft(x::Float64,start::Float64,per::Float64)
         if start >= x
@@ -84,35 +90,36 @@ function read_ocean_velocities(howmany, ww_ocean_data;
         return start
     end
 
-    #Indexes below are 0-based!
+    #Indices below are 0-based!
 
     if bounds_given
-        llxi = getIndex(LL[1], Lon[1],Lon[1]+360,nx_full,periodic)[1]
+        llxi = getIndex(LL[1], Lon[1],Lon[1]+360,0.0,nx_full,periodic)[1]
         llx = periodicGoLeft(LL[1], Lon[llxi],360.0)
 
-        urxi = mod(getIndex(UR[1], Lon[1],Lon[1]+360,nx_full,periodic)[1] + 1, nx_full)
+        urxi = mod(getIndex(UR[1], Lon[1],Lon[1]+360,0.0,nx_full,periodic)[1] + 1, nx_full)
         nx_small = mod(urxi - llxi,nx_full)
-        urx = llxi + n_small*(Lon[2] - Lon[1])
+        urx = llxi + nx_small*(Lon[2] - Lon[1])
 
-        llyi = getIndex(LL[2], Lat[1],Lat[1]+180,ny_full,periodic)[1]
+        llyi = getIndex(LL[2], Lat[1],Lat[1]+180,0.0,ny_full,periodic)[1]
         lly = periodicGoLeft(LL[2], Lat[llyi],180.0)
 
-        uryi = mod(getIndex(UR[2], Lat[1],Lat[1]+180,ny_full,periodic)[1] + 1, ny_full)
+        uryi = mod(getIndex(UR[2], Lat[1],Lat[1]+180,0.0,ny_full,periodic)[1] + 1, ny_full)
         ny_small = mod(uryi - llyi,ny_full)
         ury = llyi + ny_small*(Lat[2] - Lat[1])
     else
         llxi = 0
         llx = Lon[1]
-        urxi = nx_full
+        #urxi = nx_full-1
         urx = Lon[1] + 360.0
         nx_small = nx_full
 
         llyi = 0
         lly = Lat[1]
-        uryi = ny_full
+        #uryi = ny_full-1
         ury = Lat[1] + 180.0
         ny_small = ny_full
     end
+
         
     times = zeros(howmany)
     nt = size(times)[1]
@@ -125,9 +132,9 @@ function read_ocean_velocities(howmany, ww_ocean_data;
     LatS .= Lat
     timesS .= times
 
-    Us = arraycons(nx_full, ny_full, nt)
-    Vs = arraycons(nx_full, ny_full, nt)
-    Ust1S = arraycons(nx_full, ny_full, 1)
+    Us = arraycons(nx_small, ny_small, nt)
+    Vs = arraycons(nx_small, ny_small, nt)
+    Ust1S = arraycons(nx_small, ny_small, 1)
 
     numfound = 0
     skipped = 0
@@ -150,7 +157,7 @@ function read_ocean_velocities(howmany, ww_ocean_data;
         d = NCD.Dataset(fname)
         U, t = loadField(d,fname, "ugos")
         V, _ = loadField(d,fname, "vgos")
-        rescaleUV(U, V, Lon, Lat,Us,Vs,numfound,remove_nan)
+        rescaleUV(U, V, Lon, Lat,Us,Vs,numfound,remove_nan,llxi,llyi)
         close(d)
         times[numfound] = t
     end
@@ -253,8 +260,8 @@ function getPFull(foldername;
                 remove_nan=true,
                 start_date=nothing,
                 nskip=0,
-                b=(0,0,1),
-                arraycons=SharedArray{Float64})
+                arraycons=SharedArray{Float64},
+                boundaryT=flat)
     if sshs
         Lon, Lat, ssh_vals, times, sshsT1 = read_ssh(ndays, foldername;
             remove_nan=remove_nan, start_date=start_date, nskip=nskip, arraycons=arraycons)
@@ -264,6 +271,9 @@ function getPFull(foldername;
             remove_nan=remove_nan, start_date=start_date, nskip=nskip, arraycons=arraycons)
         ssh_vals  = nothing
     end
+
+    b=(periodic,periodic,boundaryT)
+
     nx = length(Lon)
     ny = length(Lat)
     nt = length(times)
@@ -286,21 +296,24 @@ end
 Like `getPFull`, but only loads part of the dataset in space
 (specifiable via lower left corner `LL_space` and upper right corner `UR_space`). 
 """
-function getPPart(foldername;
+function getPPart(foldername,LL_space,UR_space;
                 ndays=90,
                 sshs=false,
                 remove_nan=true,
                 start_date=nothing,
                 nskip=0,
-                b=(0,0,1),
-                arraycons=SharedArray{Float64})
+                arraycons=SharedArray{Float64},
+                boundaryT=flat)
+
+
+    b=(semiperiodic,semiperiodic,boundaryT)
 
     if sshs
         Lon, Lat, ssh_vals, times, sshsT1 = read_ssh(ndays, foldername;
             remove_nan=remove_nan, start_date=start_date, nskip=nskip, arraycons=arraycons)
         Us, Vs = nothing, nothing
     else
-        Lon, Lat, Us, Vs, times, Ust1 = read_ocean_velocities(ndays, foldername;
+        Lon, Lat, Us, Vs, times, Ust1 = read_ocean_velocities(ndays, foldername;LL_space=LL_space,UR_space=UR_space,
             remove_nan=remove_nan, start_date=start_date, nskip=nskip, arraycons=arraycons)
         ssh_vals  = nothing
     end
